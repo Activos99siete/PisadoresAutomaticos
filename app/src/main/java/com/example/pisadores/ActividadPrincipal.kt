ad80a204e98e283df6fbc7c6a3278ac86d1f35f9
Activos99siete <nasaspacexmusic@gmail.com>
2026-08-15 23:41:47 -0600

    Create ActividadPrincipal.kt

---
+++ app/src/main/java/com/example/pisadores/ActividadPrincipal.kt
@@ -0,0 +1,13 @@
+package com.example.pisadores
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActividadPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.actividad_principal)
+    }
+}
